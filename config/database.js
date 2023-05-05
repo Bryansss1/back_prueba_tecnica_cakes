f587ac28d5d3ef5310e808d0eb409e25be50def9
Bryansss1 <gregoria1540@gmail.com>
2023-05-05 03:08:39 -0400

    hecho

--- config/database.js
+++ config/database.js
@@ -1,3 +1,24 @@
+const { parse } = require("pg-connection-string");
+
+module.exports = ({ env }) => {
+  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+  
+  return {
+    connection: {
+      client: 'postgres',
+      connection: {
+        host,
+        port,
+        database,
+        user,
+        password,
+        ssl: { rejectUnauthorized: false },
+      },
+      debug: false,
+    },
+  }
+};
+/*
 const path = require('path');
 
 module.exports = ({ env }) => {
@@ -90,3 +111,4 @@ module.exports = ({ env }) => {
     },
   };
 };
+*/
